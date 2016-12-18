3c035968aca5f840da0c927e809b3e98fe86b6b1
Rahul RV <rahul@gametime.co>
2016-12-18 12:50:51 -0800

    Initial Commit

---
+++ app/src/main/java/com/kotlinsample/rahulrv/kotlinsample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.kotlinsample.rahulrv.kotlinsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity);
+    }
+}
