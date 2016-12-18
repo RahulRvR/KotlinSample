b7ba2f816117dbe3a56fecd02b5abf2c3cc9f8a9
Rahul RV <rahul@gametime.co>
2016-12-18 14:04:33 -0800

    Kotlin extentsion - No more findByViewgit status

--- app/src/main/java/com/kotlinsample/rahulrv/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/kotlinsample/rahulrv/kotlinsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotlinsample.rahulrv.kotlinsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.*
 
 /**
  *
@@ -12,5 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity);
+        text.setText("Hello World!")
     }
 }
