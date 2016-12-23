c3a4c50acaf10deea5273f1972a14cc5be86569f
Rahul RV <rahul@gametime.co>
2016-12-22 21:23:48 -0800

    Kotlin extension functions

--- app/src/main/java/com/kotlinsample/rahulrv/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/kotlinsample/rahulrv/kotlinsample/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.kotlinsample.rahulrv.kotlinsample
 
+import android.app.Activity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_activity.*
+import android.widget.Toast
 
 /**
  *
@@ -13,6 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity);
-        text.setText("Hello World!")
+        toast("Hello world");
+    }
+
+
+    fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
+    }
+
+    fun Activity.switchFragment(fragment: Fragment, tag: String = fragment.tag) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment, tag)
     }
 }
